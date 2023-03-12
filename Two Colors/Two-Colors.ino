7073279f4292419de5249b39b11b708f599320be
YeseniaMartinez24 <114689978+YeseniaMartinez24@users.noreply.github.com>
2023-03-12 12:16:43 -0600

    Add files via upload

---
+++ Two Colors/Two-Colors.ino
@@ -0,0 +1,19 @@
+int Led_Red = 4;
+int Led_Green = 2;
+
+void setup() {
+  pinMode(Led_Red, OUTPUT); 
+  pinMode(Led_Green, OUTPUT); 
+}
+void loop() {
+   for (int val = 255; val > 0; val--) {
+      analogWrite(Led_Green, val);
+      analogWrite(Led_Red, 255-val);
+      delay(15);
+   }
+   for (int val = 0; val < 255; val++) {
+      analogWrite(Led_Green, val);
+      analogWrite(Led_Red, 255-val);
+      delay(15);
+   }
+}
