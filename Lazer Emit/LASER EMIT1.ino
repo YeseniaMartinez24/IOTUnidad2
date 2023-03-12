d1596e237247d585abd06ad60b3608362f49d9d0
YeseniaMartinez24 <114689978+YeseniaMartinez24@users.noreply.github.com>
2023-03-12 12:09:11 -0600

    Add files via upload

---
+++ Lazer Emit/LASER EMIT1.ino
@@ -0,0 +1,14 @@
+int pin = 2;
+
+void setup() {
+  pinMode(pin, OUTPUT);  //definir pin como salida
+}
+ 
+void loop(){
+  digitalWrite(pin, HIGH);   // poner el Pin en HIGH
+  delay(5000);               // esperar 5 segundos
+  digitalWrite(pin, LOW);    // poner el Pin en LOW
+  delay(2000);               // esperar 20 segundos
+}
+
+//LASER EMIT
